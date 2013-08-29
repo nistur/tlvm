947c0f94af98aa56a82c6c90592e3108a551bc18
Philipp Geyer <philipp@geyer.co.uk>
2013-08-29 20:58:31 +0100

    First pass at ANA/ANI

--- src/include/tlvm_instructions.h
+++ src/include/tlvm_instructions.h
@@ -14,17 +14,21 @@
 #define TLVM_MOV_L        0x75
 #define TLVM_MOV_A        0x77
 
+#define TLVM_ANA_B        0xA0
+#define TLVM_ANA_C        0xA1
+#define TLVM_ANA_D        0xA2
+#define TLVM_ANA_E        0xA3
+#define TLVM_ANA_H        0xA4
+#define TLVM_ANA_L        0xA5
+#define TLVM_ANA_M        0xA6
+#define TLVM_ANA_A        0xA7
+#define TLVM_ANI          0xE6
+
 /*********************************************
  * ALU instruction opcodes
  *********************************************/
  #define TLVM_ADD_B       0x80 // add register B to A
  #define TLVM_ADD_C       0x81 // add register C to A
-
-/*********************************************
- * FPU instruction opcodes
- *********************************************/
- #define TLVM_ADDF_RM     0xA0 // add from register to memory
- #define TLVM_ADDF_MR     0xA1 // add from memory to register
 
 
 /*********************************************
@@ -32,6 +36,8 @@
  *********************************************/
 tlvmReturn tlvmNOOP(tlvmContext* context);
 tlvmReturn tlvmMOV (tlvmContext* context);
+tlvmReturn tlvmANA (tlvmContext* context);
+tlvmReturn tlvmANI (tlvmContext* context);
 
 /*********************************************
  * ALU instructions
--- src/tlvm_core.c
+++ src/tlvm_core.c
@@ -7,6 +7,7 @@ tlvmReturn tlvmInitCore(tlvmContext* context)
 		tlvmReturnCode(NO_CONTEXT);
 
     context->m_InstructionSet[TLVM_NOOP] = tlvmNOOP;
+
     context->m_InstructionSet[TLVM_MOV_B] = tlvmMOV;
     context->m_InstructionSet[TLVM_MOV_C] = tlvmMOV;
     context->m_InstructionSet[TLVM_MOV_D] = tlvmMOV;
@@ -14,6 +15,17 @@ tlvmReturn tlvmInitCore(tlvmContext* context)
     context->m_InstructionSet[TLVM_MOV_H] = tlvmMOV;
     context->m_InstructionSet[TLVM_MOV_L] = tlvmMOV;
     context->m_InstructionSet[TLVM_MOV_A] = tlvmMOV;
+
+    context->m_InstructionSet[TLVM_ANA_B] = tlvmANA;
+    context->m_InstructionSet[TLVM_ANA_C] = tlvmANA;
+    context->m_InstructionSet[TLVM_ANA_D] = tlvmANA;
+    context->m_InstructionSet[TLVM_ANA_E] = tlvmANA;
+    context->m_InstructionSet[TLVM_ANA_H] = tlvmANA;
+    context->m_InstructionSet[TLVM_ANA_L] = tlvmANA;
+    context->m_InstructionSet[TLVM_ANA_M] = tlvmANA;
+    context->m_InstructionSet[TLVM_ANA_A] = tlvmANA;
+
+    context->m_InstructionSet[TLVM_ANI]   = tlvmANI;
 
 	tlvmReturnCode(SUCCESS);
 }
@@ -70,6 +82,73 @@ tlvmReturn tlvmMOV(tlvmContext* context)
 
 	// size of instruction    = 1
 	context->m_ProgramCounter += 1;
+
+	tlvmReturnCode(SUCCESS);
+}
+
+tlvmReturn tlvmANA(tlvmContext* context)
+{
+	if(context == NULL)
+		tlvmReturnCode(NO_CONTEXT);
+
+	tlvmByte opcode = context->m_Program[context->m_ProgramCounter+0];
+
+	tlvmByte* src = NULL;
+
+	switch(opcode)
+	{
+	case TLVM_ANA_B:
+		src = &context->m_Registers[TLVM_REG_B];
+	break;
+	case TLVM_ANA_C:
+		src = &context->m_Registers[TLVM_REG_C];
+	break;
+	case TLVM_ANA_D:
+		src = &context->m_Registers[TLVM_REG_D];
+	break;
+	case TLVM_ANA_E:
+		src = &context->m_Registers[TLVM_REG_E];
+	break;
+	case TLVM_ANA_H:
+		src = &context->m_Registers[TLVM_REG_H];
+	break;
+	case TLVM_ANA_L:
+		src = &context->m_Registers[TLVM_REG_L];
+	break;
+	case TLVM_ANA_M:
+		{
+			tlvmShort addr = ((tlvmShort)context->m_Registers[TLVM_REG_H]) << 8 | (tlvmShort)context->m_Registers[TLVM_REG_L];
+			src = &context->m_Memory[addr];
+		}
+	break;
+	case TLVM_ANA_A:
+		src = &context->m_Registers[TLVM_REG_A];
+	break;
+	}
+
+	if(src == NULL)
+		tlvmReturnCode(INVALID_INPUT);
+
+	context->m_Registers[TLVM_REG_A] &= *src;
+
+	// size of instruction    = 1
+	context->m_ProgramCounter += 1;
+
+	tlvmReturnCode(SUCCESS);
+}
+
+tlvmReturn tlvmANI(tlvmContext* context)
+{
+	if(context == NULL)
+		tlvmReturnCode(NO_CONTEXT);
+
+	tlvmByte operand = context->m_Program[context->m_ProgramCounter+1];
+
+	context->m_Registers[TLVM_REG_A] &= operand;
+
+	// size of instruction    = 1
+	// size of operand        = 1
+	context->m_ProgramCounter += 2;
 
 	tlvmReturnCode(SUCCESS);
 }
