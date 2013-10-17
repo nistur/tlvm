29dd731808a9a7dc6241219ad707e51a50617c2f
Philipp Geyer <philipp@geyer.co.uk>
2013-10-17 19:10:58 +0100

    Added MOV and MVI instructions to 8080 metadata

--- src/8080/8080_debug.c
+++ src/8080/8080_debug.c
@@ -1,7 +1,26 @@
 #include "tlvm_internal.h"
 
+#include <stdio.h>
+
 #ifdef  TLVM_HAS_8080
 #ifdef  TLVM_DEBUG
+
+#define TLVM_DEBUG_SIMPLE_OP(op, instr) \
+if(opcode == TLVM_##op) \
+{ \
+	sprintf((char*)*instruction, instr); \
+	tlvmReturnCode(SUCCESS); \
+}
+
+#define TLVM_DEBUG_SIMPLE_OPCODE(op) TLVM_DEBUG_SIMPLE_OP(op, #op)
+
+#define TLVM_DEBUG_8BIT_OP(op, instr) \
+if(opcode == TLVM_##op) \
+{ \
+	TLVM_GET_OP(operand, 1); \
+	sprintf((char*)*instruction, "%s 0x%x", instr, operand); \
+	tlvmReturnCode(SUCCESS); \
+}
 
 tlvmReturn tlvm8080DebugGetInstruction(tlvmContext* context, tlvmChar** instruction)
 {
@@ -9,18 +28,90 @@ tlvmReturn tlvm8080DebugGetInstruction(tlvmContext* context, tlvmChar** instruct
 		tlvmReturnCode(NO_CONTEXT);
 
     TLVM_GET_OP(opcode, 0);
-    if(opcode == TLVM_NOP)
-    {
-    	sprintf(*instruction, "NOP");
-		tlvmReturnCode(SUCCESS);
-    }
-    else if(opcode = TLVM_MOV_BB)
-    {
-    	sprintf(*instruction, "MOV B B");
-    	tlvmReturnCode(SUCCESS);
-    }
 
-    sprintf(*instruction, "UNKNOWN");
+    TLVM_DEBUG_SIMPLE_OPCODE(NOP);
+    TLVM_DEBUG_SIMPLE_OP(MOV_BB, "MOV B B");
+    TLVM_DEBUG_SIMPLE_OP(MOV_BC, "MOV B C");
+    TLVM_DEBUG_SIMPLE_OP(MOV_BD, "MOV B D");
+    TLVM_DEBUG_SIMPLE_OP(MOV_BE, "MOV B E");
+    TLVM_DEBUG_SIMPLE_OP(MOV_BH, "MOV B H");
+    TLVM_DEBUG_SIMPLE_OP(MOV_BL, "MOV B L");
+    TLVM_DEBUG_SIMPLE_OP(MOV_BM, "MOV B M");
+    TLVM_DEBUG_SIMPLE_OP(MOV_BA, "MOV B A");
+
+    TLVM_DEBUG_SIMPLE_OP(MOV_CB, "MOV C B");
+    TLVM_DEBUG_SIMPLE_OP(MOV_CC, "MOV C C");
+    TLVM_DEBUG_SIMPLE_OP(MOV_CD, "MOV C D");
+    TLVM_DEBUG_SIMPLE_OP(MOV_CE, "MOV C E");
+    TLVM_DEBUG_SIMPLE_OP(MOV_CH, "MOV C H");
+    TLVM_DEBUG_SIMPLE_OP(MOV_CL, "MOV C L");
+    TLVM_DEBUG_SIMPLE_OP(MOV_CM, "MOV C M");
+    TLVM_DEBUG_SIMPLE_OP(MOV_CA, "MOV C A");
+
+    TLVM_DEBUG_SIMPLE_OP(MOV_DB, "MOV D B");
+    TLVM_DEBUG_SIMPLE_OP(MOV_DC, "MOV D C");
+    TLVM_DEBUG_SIMPLE_OP(MOV_DD, "MOV D D");
+    TLVM_DEBUG_SIMPLE_OP(MOV_DE, "MOV D E");
+    TLVM_DEBUG_SIMPLE_OP(MOV_DH, "MOV D H");
+    TLVM_DEBUG_SIMPLE_OP(MOV_DL, "MOV D L");
+    TLVM_DEBUG_SIMPLE_OP(MOV_DM, "MOV D M");
+    TLVM_DEBUG_SIMPLE_OP(MOV_DA, "MOV D A");
+
+    TLVM_DEBUG_SIMPLE_OP(MOV_EB, "MOV E B");
+    TLVM_DEBUG_SIMPLE_OP(MOV_EC, "MOV E C");
+    TLVM_DEBUG_SIMPLE_OP(MOV_ED, "MOV E D");
+    TLVM_DEBUG_SIMPLE_OP(MOV_EE, "MOV E E");
+    TLVM_DEBUG_SIMPLE_OP(MOV_EH, "MOV E H");
+    TLVM_DEBUG_SIMPLE_OP(MOV_EL, "MOV E L");
+    TLVM_DEBUG_SIMPLE_OP(MOV_EM, "MOV E M");
+    TLVM_DEBUG_SIMPLE_OP(MOV_EA, "MOV E A");
+
+    TLVM_DEBUG_SIMPLE_OP(MOV_HB, "MOV H B");
+    TLVM_DEBUG_SIMPLE_OP(MOV_HC, "MOV H C");
+    TLVM_DEBUG_SIMPLE_OP(MOV_HD, "MOV H D");
+    TLVM_DEBUG_SIMPLE_OP(MOV_HE, "MOV H E");
+    TLVM_DEBUG_SIMPLE_OP(MOV_HH, "MOV H H");
+    TLVM_DEBUG_SIMPLE_OP(MOV_HL, "MOV H L");
+    TLVM_DEBUG_SIMPLE_OP(MOV_HM, "MOV H M");
+    TLVM_DEBUG_SIMPLE_OP(MOV_HA, "MOV H A");
+
+    TLVM_DEBUG_SIMPLE_OP(MOV_LB, "MOV L B");
+    TLVM_DEBUG_SIMPLE_OP(MOV_LC, "MOV L C");
+    TLVM_DEBUG_SIMPLE_OP(MOV_LD, "MOV L D");
+    TLVM_DEBUG_SIMPLE_OP(MOV_LE, "MOV L E");
+    TLVM_DEBUG_SIMPLE_OP(MOV_LH, "MOV L H");
+    TLVM_DEBUG_SIMPLE_OP(MOV_LL, "MOV L L");
+    TLVM_DEBUG_SIMPLE_OP(MOV_LM, "MOV L M");
+    TLVM_DEBUG_SIMPLE_OP(MOV_LA, "MOV L A");
+
+    TLVM_DEBUG_SIMPLE_OP(MOV_MB, "MOV M B");
+    TLVM_DEBUG_SIMPLE_OP(MOV_MC, "MOV M C");
+    TLVM_DEBUG_SIMPLE_OP(MOV_MD, "MOV M D");
+    TLVM_DEBUG_SIMPLE_OP(MOV_ME, "MOV M E");
+    TLVM_DEBUG_SIMPLE_OP(MOV_MH, "MOV M H");
+    TLVM_DEBUG_SIMPLE_OP(MOV_ML, "MOV M L");
+    //TLVM_DEBUG_SIMPLE_OP(MOV_MM, "MOV M M");
+    TLVM_DEBUG_SIMPLE_OP(MOV_MA, "MOV M A");
+
+    TLVM_DEBUG_SIMPLE_OP(MOV_AB, "MOV A B");
+    TLVM_DEBUG_SIMPLE_OP(MOV_AC, "MOV A C");
+    TLVM_DEBUG_SIMPLE_OP(MOV_AD, "MOV A D");
+    TLVM_DEBUG_SIMPLE_OP(MOV_AE, "MOV A E");
+    TLVM_DEBUG_SIMPLE_OP(MOV_AH, "MOV A H");
+    TLVM_DEBUG_SIMPLE_OP(MOV_AL, "MOV A L");
+    TLVM_DEBUG_SIMPLE_OP(MOV_AM, "MOV A M");
+    TLVM_DEBUG_SIMPLE_OP(MOV_AA, "MOV A A");
+
+    TLVM_DEBUG_8BIT_OP(MVI_B, "MVI B");
+    TLVM_DEBUG_8BIT_OP(MVI_C, "MVI C");
+    TLVM_DEBUG_8BIT_OP(MVI_D, "MVI D");
+    TLVM_DEBUG_8BIT_OP(MVI_E, "MVI E");
+    TLVM_DEBUG_8BIT_OP(MVI_H, "MVI H");
+    TLVM_DEBUG_8BIT_OP(MVI_L, "MVI L");
+    TLVM_DEBUG_8BIT_OP(MVI_M, "MVI M");
+    TLVM_DEBUG_8BIT_OP(MVI_A, "MVI A");
+
+    sprintf((char*)*instruction, "UNKNOWN");
     tlvmReturnCode(UNKNOWN_INSTRUCTION);
 }
 
