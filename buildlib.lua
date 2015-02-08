190e85010e9bda7c8a84166d23eb7abe9ad201a6
Philipp Geyer <philipp@geyer.co.uk>
2015-02-08 19:56:30 +0000

    trying to fix include issues

--- buildlib.lua
+++ buildlib.lua
@@ -4,11 +4,13 @@ project "tlvm"
 language "C"
 kind "StaticLib"
 defines { "TLVM_DEBUG" } -- this is not debug symbols, this is the interface for the tlvm debugger
+includedirs { "include", "src/include" }
 files { "src/**.c", "src/**.cpp" }
 
 project "tlvm-lite"
 language "C"
 kind "StaticLib"
+includedirs { "include", "src/include" }
 files { "src/**.c", "src/**.cpp" }
 
 --project "tlvm-dynamic"
